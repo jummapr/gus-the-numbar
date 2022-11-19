a173f4d479a6ebcc4b3c9c7c363188ee98e41523
jumma <aarifhingorja4@gmail.com>
2022-11-19 12:56:00 +0530

    fist commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-console.log('Hello World!');
+console.log("Hello World!");
 
 let computerguess;
 let userguess = [];
@@ -9,84 +9,69 @@ let inputbox = document.getElementById("input-box");
 let new_game = document.getElementById("new_game");
 
 const init = () => {
-    computerguess = Math.floor(Math.random() * 100);
+  computerguess = Math.floor(Math.random() * 100);
 
-    document.getElementById("game-button").style.display = 'none';
-    document.getElementById("gameare").style.display = 'none';
+  document.getElementById("game-button").style.display = "none";
+  document.getElementById("gameare").style.display = "none";
 };
 
 const startgame = () => {
-    document.getElementById("welcomescreen").style.display = 'none';
-    document.getElementById("gameare").style.display = 'block';
-
+  document.getElementById("welcomescreen").style.display = "none";
+  document.getElementById("gameare").style.display = "block";
 };
 
 const stratnewgame = () => {
-    document.getElementById("game-button").style.display = 'inline';
-    document.getElementById("input-box").setAttribute = 'disebal', 'true';
-}
+  document.getElementById("game-button").style.display = "inline";
+  (document.getElementById("input-box").setAttribute = "disebal"), "true";
+};
 const newgame = () => {
-    window.location.reload();
-
-}
+  window.location.reload();
+};
 
 // main logic strat
 const cum = () => {
-    const usernumbar = Number(document.getElementById("input-box").value);
-    userguess = [...userguess, usernumbar];
-    document.getElementById("info-gues").innerHTML = userguess;
+  const usernumbar = Number(document.getElementById("input-box").value);
+  userguess = [...userguess, usernumbar];
+  document.getElementById("info-gues").innerHTML = userguess;
 
-    // value chek
+  // value chek
 
-    if (userguess.length < maxguess) {
-        if (usernumbar > computerguess) {
-
-            para.innerHTML = "Your High Guess 😲";
-            inputbox.value = "";
-
-        } else if (usernumbar < computerguess) {
-
-            para.innerHTML = "Your Low Guess 😥";
-            inputbox.value = "";
-
-        } else {
-
-            para.innerHTML = "Ha.. Ha.. Ha.. Your Winer 😁";
-            inputbox.value = "";
-            stratnewgame();
-
-        }
+  if (userguess.length < maxguess) {
+    if (usernumbar > computerguess) {
+      para.innerHTML = "Your High Guess 😲";
+      inputbox.value = "";
+    } else if (usernumbar < computerguess) {
+      para.innerHTML = "Your Low Guess 😥";
+      inputbox.value = "";
     } else {
-        if (usernumbar > computerguess) {
-
-            para.innerHTML = `your loose  it's curect numbar was ${computerguess}`;
-            inputbox.value = "";
-            stratnewgame();
-
-        } else if (usernumbar < computerguess) {
-
-            para.innerHTML = `your loose || it's curect numbar was ${computerguess}`;
-            inputbox.value = "";
-            stratnewgame();
-
-        } else {
-
-            para.innerHTML = "Ha.. Ha.. Ha.. Your Winer 😁";
-            inputbox.value = "";
-            stratnewgame();
-
-        }
+      para.innerHTML = "Ha.. Ha.. Ha.. Your Winer 😁";
+      inputbox.value = "";
+      stratnewgame();
     }
+  } else {
+    if (usernumbar > computerguess) {
+      para.innerHTML = `your loose  it's curect numbar was ${computerguess}`;
+      inputbox.value = "";
+      stratnewgame();
+    } else if (usernumbar < computerguess) {
+      para.innerHTML = `your loose || it's curect numbar was ${computerguess}`;
+      inputbox.value = "";
+      stratnewgame();
+    } else {
+      para.innerHTML = "Ha.. Ha.. Ha.. Your Winer 😁";
+      inputbox.value = "";
+      stratnewgame();
+    }
+  }
 
-    document.getElementById("info-num").innerHTML = userguess.length;
-
-}
+  document.getElementById("info-num").innerHTML = userguess.length;
+};
 
 btn_1.addEventListener("click", () => {
-    maxguess = 10;
-    startgame();
+  maxguess = 10;
+  startgame();
 });
 btn_2.addEventListener("click", () => {
-    maxguess = 5;
-    startgame();
+  maxguess = 5;
+  startgame();
 });
